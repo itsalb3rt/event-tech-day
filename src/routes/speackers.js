523194b6c5463667e36ced79230318b85dd47f2d
itsalb3rt <itsalb3rt@gmail.com>
2019-11-09 17:16:52 -0400

    minor modifications

---
+++ src/routes/speackers.js
@@ -0,0 +1,77 @@
+import express from 'express'
+import speakersModel from './../models/speakers'
+
+var routerSpeakers = express.Router();
+
+routerSpeakers.get('/', (req, res) => {
+
+    speakersModel.findAll()
+        .then(speakers => {
+            res.send(speakers);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+
+});
+
+routerSpeakers.get('/:id', (req, res) => {
+    speakersModel.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(talk => {
+            res.send(talk);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+});
+
+routerSpeakers.post('/', (req, res) => {
+    let newEvent = req.body;
+    newEvent.createAt = Date.now();
+
+    speakersModel.create(newEvent)
+        .then(talk => {
+            res.send(talk);
+        })
+        .catch(error => {
+            res.status(400).send(error.errors);
+        })
+});
+
+routerSpeakers.put('/:id', (req, res) => {
+    let updateEvent = req.body;
+    updateEvent.updatedAt = Date.now();
+
+    speakersModel.update(updateEvent, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(talk => {
+            res.send(talk);
+        })
+        .catch(error => {
+            res.status(400).send(error.errors);
+        })
+})
+
+routerSpeakers.delete('/:id', (req, res) => {
+
+    speakersModel.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(() => {
+            res.send('deleted');
+        })
+        .catch(error => {
+            res.status(400).send(error.errors);
+        })
+})
+
+export default routerSpeakers;
