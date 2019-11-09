196b0c091cd9ce5365448854910262497b83c31f
itsalb3rt <itsalb3rt@gmail.com>
2019-11-09 16:53:05 -0400

    add find by id talk

--- src/routes/talks.js
+++ src/routes/talks.js
@@ -16,7 +16,17 @@ routerTalks.get('/', (req, res) => {
 });
 
 routerTalks.get('/:id', (req, res) => {
-    res.send('talk id ' + req.params.id)
+    talksModel.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(talk => {
+            res.send(talk);
+        })
+        .catch(error => {
+            res.send(error);
+        })
 });
 
 routerTalks.post('/', (req, res) => {
@@ -24,42 +34,50 @@ routerTalks.post('/', (req, res) => {
     newEvent.createAt = Date.now();
 
     talksModel.create({
-        name: newEvent.name,
-        tags: newEvent.tags,
-        id_speaker: newEvent.id_speaker,
-        time: newEvent.time,
-        createdAt: newEvent.createAt
-    })
-    .then(talk => {
-        res.send(talk);
-    })
-    .catch( error => {
-        res.status(400).send('Error in insert new record');
-    })
+            name: newEvent.name,
+            tags: newEvent.tags,
+            id_speaker: newEvent.id_speaker,
+            time: newEvent.time,
+            createdAt: newEvent.createAt
+        })
+        .then(talk => {
+            res.send(talk);
+        })
+        .catch(error => {
+            res.status(400).send('Error in insert new record');
+        })
 });
 
 routerTalks.put('/:id', (req, res) => {
     let updateEvent = req.body;
     updateEvent.updatedAt = Date.now();
 
-    talksModel.update(updateEvent,{where:{id:req.params.id}})
-    .then(talk => {
-        res.send(talk);
-    })
-    .catch( error => {
-        res.status(400).send('Error in insert new record');
-    })
+    talksModel.update(updateEvent, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(talk => {
+            res.send(talk);
+        })
+        .catch(error => {
+            res.status(400).send('Error in insert new record');
+        })
 })
 
 routerTalks.delete('/:id', (req, res) => {
 
-    talksModel.destroy({where:{id:req.params.id}})
-    .then( () => {
-        res.send('deleted');
-    })
-    .catch( error => {
-        res.status(400).send(error);
-    })
+    talksModel.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(() => {
+            res.send('deleted');
+        })
+        .catch(error => {
+            res.status(400).send(error);
+        })
 })
 
 export default routerTalks;
