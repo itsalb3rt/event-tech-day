973a5bf68b7dc9e3de076d41a835d3072785a07d
itsalb3rt <itsalb3rt@gmail.com>
2019-11-12 14:33:55 -0400

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import routes from './routes'
+import cors from 'cors'
 
 const app = express()
 const port = 3000
 //middleware
 app.use(bodyParser.json())
+app.use(cors({
+    origin: '*'
+  }))
 app.use('/api',routes)
 
 app.get('/', (req, res) => {
