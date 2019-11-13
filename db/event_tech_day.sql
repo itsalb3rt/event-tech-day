9660296a443d47325fc8402be808b0a24cfad3f1
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-13 15:14:17 -0400

    minor modification on schema

--- db/event_tech_day.sql
+++ db/event_tech_day.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 09-11-2019 a las 22:40:32
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.9
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 13-11-2019 a las 15:13:39
+-- Versión del servidor: 5.7.14
+-- Versión de PHP: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,22 +30,16 @@ USE `event_tech_day`;
 -- Estructura de tabla para la tabla `speakers`
 --
 
-CREATE TABLE `speakers` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `speakers`;
+CREATE TABLE IF NOT EXISTS `speakers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(75) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(75) COLLATE utf8mb4_unicode_ci NOT NULL,
   `github_account` varchar(75) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `speakers`
---
-
-INSERT INTO `speakers` (`id`, `name`, `email`, `github_account`, `createdAt`, `updatedAt`) VALUES
-(2, 'Albert Hidalgo', 'itsalb3rt@gmail.com', 'itsalb3rt', '2019-11-09 21:39:48', '0000-00-00 00:00:00'),
-(3, 'Albert Hidalgo', 'itsalb3rt@gmail.com', 'itsalb3rt', '2019-11-09 21:40:08', '2019-11-09 21:40:08');
 
 -- --------------------------------------------------------
 
@@ -53,54 +47,17 @@ INSERT INTO `speakers` (`id`, `name`, `email`, `github_account`, `createdAt`, `u
 -- Estructura de tabla para la tabla `talks`
 --
 
-CREATE TABLE `talks` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `talks`;
+CREATE TABLE IF NOT EXISTS `talks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `tags` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id_speaker` int(11) NOT NULL,
+  `speakerId` int(11) NOT NULL,
   `time` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `talks`
---
-
-INSERT INTO `talks` (`id`, `name`, `tags`, `id_speaker`, `time`, `createdAt`, `updatedAt`) VALUES
-(3, 'Title', 'foo,bar', 1, '08:00 AM to 09:00 AM', '2019-11-09 20:49:24', '2019-11-09 20:49:24');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `speakers`
---
-ALTER TABLE `speakers`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `talks`
---
-ALTER TABLE `talks`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `speakers`
---
-ALTER TABLE `speakers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `talks`
---
-ALTER TABLE `talks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
